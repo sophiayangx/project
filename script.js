ab89e9cc9b43096754d7f120826c46afe0cc62e2
sophiayangx <sopyang@nuevaschool.org>
2016-10-05 09:37:21 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("sophia".length>4)
+"sophia".length
+console.log(5<=5,6)
+
+//var submit = "character"
+////submit.click(function(adsf) {
+//// return joey;
+//})
+
+//confirm("Joey!");
+//prompt("Which Friends character are you?");
+
+var submitButton = $("submit-button")
+submitButton.click(function() {	
+  $("#list-display").getElementbyId("");
+});
+
+function() {
+  document.getElementById("submit-button").click();
+}
